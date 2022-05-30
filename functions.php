a22d60d037d9931de24da349521551a673abee83
matheo <matheomatheo07@gmail.com>
2022-05-30 16:07:38 -0400

    Disable rendering characters to emojis

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions and definitions
  *
@@ -13,31 +14,34 @@
  *
  * @since 0.1
  */
-define( 'GASPESIE_VERSION', wp_get_theme()->get( 'Version' ) );
+define('GASPESIE_VERSION', wp_get_theme()->get('Version'));
 
 /**
  * Add theme support.
  */
-function gaspesie_setup() {
-	add_theme_support( 'wp-block-styles' );
-	add_editor_style( './assets/css/style-shared.min.css' );
+function gaspesie_setup()
+{
+	add_theme_support('wp-block-styles');
+	add_editor_style('./assets/css/style-shared.css');
+	add_editor_style('./assets/css/block-styles.css');
+	add_editor_style('style-editor.css');
 
 	/*
 	 * Load additional block styles.
 	 * See details on how to add more styles in the readme.txt.
 	 */
-	$styled_blocks = [ 'button', 'file', 'quote', 'search' ];
-	foreach ( $styled_blocks as $block_name ) {
+	$styled_blocks = ['button', 'file', 'quote', 'search'];
+	foreach ($styled_blocks as $block_name) {
 		$args = array(
 			'handle' => "gaspesie-$block_name",
-			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.min.css" ),
-			'path'   => get_theme_file_path( "assets/css/blocks/$block_name.min.css" ),
+			'src'    => get_theme_file_uri("assets/css/blocks/$block_name.css"),
+			'path'   => get_theme_file_path("assets/css/blocks/$block_name.css"),
 		);
 		// Replace the "core" prefix if you are styling blocks from plugins.
-		wp_enqueue_block_style( "core/$block_name", $args );
+		wp_enqueue_block_style("core/$block_name", $args);
 	}
 }
-add_action( 'after_setup_theme', 'gaspesie_setup' );
+add_action('after_setup_theme', 'gaspesie_setup');
 
 /**
  * Enqueue the CSS files.
@@ -46,7 +50,8 @@ add_action( 'after_setup_theme', 'gaspesie_setup' );
  *
  * @return void
  */
-function gaspesie_styles() {
+function gaspesie_styles()
+{
 	wp_enqueue_style(
 		'gaspesie-style',
 		get_stylesheet_uri(),
@@ -55,21 +60,49 @@ function gaspesie_styles() {
 	);
 	wp_enqueue_style(
 		'gaspesie-shared-styles',
-		get_theme_file_uri( 'assets/css/style-shared.min.css' ),
+		get_theme_file_uri('assets/css/style-shared.css'),
 		[],
 		GASPESIE_VERSION
 	);
 	wp_enqueue_style(
 		'gaspesie-block-styles',
-		get_theme_file_uri( 'assets/css/block-styles.css' ),
+		get_theme_file_uri('assets/css/block-styles.css'),
 		[],
 		GASPESIE_VERSION
 	);
 }
-add_action( 'wp_enqueue_scripts', 'gaspesie_styles' );
+add_action('wp_enqueue_scripts', 'gaspesie_styles');
 
 // Block style examples.
-require_once get_theme_file_path( 'inc/register-block-styles.php' );
+require_once get_theme_file_path('inc/register-block-styles.php');
 
 // Block pattern helper for the privacy policy.
-require_once get_theme_file_path( 'inc/block-pattern-helper.php' );
+require_once get_theme_file_path('inc/block-pattern-helper.php');
+
+/**
+ * Disable emojis rendering.
+ * 
+ * @link https://smartwp.com/disable-emojis-wordpress/
+ */
+add_action('init', 'disable_emojis');
+
+function disable_emojis()
+{
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+
+function disable_emojis_tinymce($plugins)
+{
+	if (is_array($plugins)) {
+		return array_diff($plugins, array('wpemoji'));
+	} else {
+		return array();
+	}
+}
